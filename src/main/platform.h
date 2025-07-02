b9ebf791ad75853ebe4037141eadc98e0d853e88
Shad Hasan <baksh12shad@gmail.com>
2025-07-02 16:02:18 +0200

    Going in testing

--- src/main/platform.h
+++ src/main/platform.h
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-void print_platform(char *buffer) {
+void print_platform(char *message_buffer) {
 	cl_int CL_err = CL_SUCCESS;
     cl_uint numPlatforms = 0;
     cl_int ret;
@@ -12,9 +12,9 @@ void print_platform(char *buffer) {
     CL_err = clGetPlatformIDs( 0, NULL, &numPlatforms );
 
     if (CL_err == CL_SUCCESS)
-        sprintf(buffer, "%u platform(s) found\n", numPlatforms);
+        sprintf(message_buffer, "%u platform(s) found\n", numPlatforms);
     else
-    	sprintf(buffer, "clGetPlatformIDs(%i)\n", CL_err);
+    	sprintf(message_buffer, "clGetPlatformIDs(%i)\n", CL_err);
 
     // Declare platform array and initialize
     cl_platform_id platforms[numPlatforms];
@@ -22,16 +22,36 @@ void print_platform(char *buffer) {
 
     // Declare device-id for fetch
     cl_uint numDevices;
-    int i;
-    char temp_buffer[400];
+
+    int i, j;
+
+    char local_dev_buf[250];
+    cl_device_id devices[20]; // maximum number of GPU devices, say, 20;
+
+    char temp_message_buffer[400];
+
     for (i = 0; i < (int)numPlatforms; i++) {
     	ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, 0, NULL, &numDevices);
     	if (CL_SUCCESS == ret) {
-    		sprintf(temp_buffer, "platform count %u, device count: %i, device found status: (%i), Device no: %u\n", numPlatforms, i, ret, numDevices);
+    		sprintf(temp_message_buffer, "In total platform: %u, platform serial count: %i, device found status in platform(0 mean good): %i, Total Device found: %u\n", numPlatforms, i, ret, numDevices);
     	} else {
-    		sprintf(temp_buffer, "device found status: (%i)\n",  ret);
+    		sprintf(temp_message_buffer, "device found status: (%i)\n",  ret);
+    		continue;
     	}
-    	strcat(buffer, temp_buffer);
+    	ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, numDevices, devices, NULL);
+		if (CL_SUCCESS != ret) {
+			continue;
+		}
+		strcat(message_buffer, temp_message_buffer);
+		for (j=0; j < (int)numDevices; j++) {
+			ret = clGetDeviceInfo(devices[j], CL_DEVICE_NAME, sizeof(local_dev_buf), local_dev_buf, NULL);
+			if (CL_SUCCESS != ret) {
+				sprintf(temp_message_buffer, "Error clGetDeviceInfo() : %d\n", ret);
+			} else {
+				sprintf(temp_message_buffer, "Found device name: : %s\n", local_dev_buf);
+			}
+		}
+    	strcat(message_buffer, temp_message_buffer);
     }
 
 }
