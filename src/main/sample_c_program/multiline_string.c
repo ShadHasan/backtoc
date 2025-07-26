7f2c9c2be6229a2f26e0d937fb42e3b352750914
Shad Hasan <baksh12shad@gmail.com>
2025-07-26 21:00:32 +0200

    Update multiline_string.c

--- src/main/sample_c_program/multiline_string.c
+++ src/main/sample_c_program/multiline_string.c
@@ -1,9 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct string_array string_array;
+
+struct string_array {
+        int count;
+        int* offset;
+        unsigned int length;
+        char* s_arr;
+};
+
+void init_string_array(string_array* string_arr) {
+        string_arr->s_arr = NULL;
+        string_arr->count = 0;
+        string_arr->length = 0;
+        string_arr->offset = (int*)malloc(sizeof(int)*1);
+        string_arr->offset[0] = 0;
+}
+
+void add_string_to_string_array(string_array* string_arr, char* string) {
+        if (string_arr->s_arr == NULL) {
+                string_arr->s_arr = (char*)malloc(sizeof(string));
+        } else {
+                string_arr->s_arr = (char*)realloc(string_arr->s_arr, sizeof(string_arr->s_arr)+sizeof(string));
+                string_arr->offset = (int*)realloc(string_arr->offset, sizeof(int)*(string_arr->count+1));
+                string_arr->offset[string_arr->count] = string_arr->length;
+        }
+        strcat(string_arr->s_arr, string);
+        string_arr->length = strlen(string_arr->s_arr);
+        string_arr->count++;
+}
+
+char* get_by_index_string_array(string_array* string_arr, int index) {
+        if(index >= string_arr->count || index < 0) {
+                return NULL;
+        } else {
+                unsigned int offset = string_arr->offset[index];
+                unsigned int limit = strlen(string_arr->s_arr);
+                if(string_arr->count > (index+1)) {
+                        limit = string_arr->offset[index+1];
+                }
+                char* temp = (char*)calloc(sizeof(char), limit-offset);
+                strncpy(temp, string_arr->s_arr + offset, limit);
+                temp[limit-offset] = '\0';
+                return temp;
+        }
+}
 
 int main() {
-        char** string1;
-        char** string2;
+        char** string1 = NULL;
+        char** string2 = NULL;
         char c = 'a';
         char* string = NULL;
         int i, j;
@@ -36,5 +83,15 @@ int main() {
 
         }
 
+        // struct is already have allocate memory. so initializing as pointer mean array of struct. Here we just need to initialize as a struct.
+        string_array str_arr1;
+        init_string_array(&str_arr1);
+        printf("initialize\n");
+        add_string_to_string_array(&str_arr1, "hello");
+        printf("ist\n");
+        add_string_to_string_array(&str_arr1, "hello2");
+
+        char* fet_str = get_by_index_string_array(&str_arr1, 0);
+        printf("%s", fet_str);
         return 0;
 }
