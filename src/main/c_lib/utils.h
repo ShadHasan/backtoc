80e5f61415048dfcc670a787602caffa6f00200d
Shad Hasan <baksh12shad@gmail.com>
2025-08-29 11:43:16 +0200

    test

--- src/main/c_lib/utils.h
+++ src/main/c_lib/utils.h
@@ -7,9 +7,10 @@
 
 bool validate_json(char* json_str){
 	int len = strlen(json_str);
-	char c;
+	char c, poped_c;
 	adv_char_stack* c_stack = init_adv_char_stack('');
 	int i;
+	bool invalid = false;
 	for(i=0;i<len;i++) {
 		c = seek_adv_char_stack(c_stack);
 		switch(json_str[i]) {
@@ -24,13 +25,31 @@ bool validate_json(char* json_str){
 				}
 				break;
 			case '"':
+				if (c != '"') {
+					push_adv_char_stack(c_stack, '"');
+				} else {
+					poped_c = pop_adv_char_stack(c_stack);
+				}
 				break;
 			case '}':
+				if (c != '{') {
+					invalid = true;
+				}
 				break;
 			case ']':
+				if (c != ']') {
+					invalid = true;
+				}
+				break;
+			case ':':
+				break;
+			case ',':
 				break;
 			default:
 				break;
+		}
+		if (invalid) {
+			break;
 		}
 	}
 }
