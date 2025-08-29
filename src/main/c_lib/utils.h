bbb5fbfb8e193d321a09c6e34dda88864d908954
Shad Hasan <baksh12shad@gmail.com>
2025-08-29 12:31:39 +0200

    in progress

--- src/main/c_lib/utils.h
+++ src/main/c_lib/utils.h
@@ -5,9 +5,23 @@
 #include <string.h>
 #include "model/common/stack.h"
 
+void tstring(char* ts, char c) {
+	int len = strlen(ts);
+	if (ts == NULL) {
+		ts = (char*)malloc(sizeof(char));
+		ts[0] = c;
+	} else {
+		ts = (char*)realloc(ts, (len+1)*sizeof(char));
+		ts[len] = c;
+	}
+
+}
+
 bool validate_json(char* json_str){
 	int len = strlen(json_str);
 	char c, poped_c;
+	bool isKey = true;
+	char* ts = NULL;
 	adv_char_stack* c_stack = init_adv_char_stack('');
 	int i;
 	bool invalid = false;
@@ -34,18 +48,31 @@ bool validate_json(char* json_str){
 			case '}':
 				if (c != '{') {
 					invalid = true;
+				} else {
+					poped_c = pop_adv_char_stack(c_stack);
 				}
 				break;
 			case ']':
 				if (c != ']') {
 					invalid = true;
+				} else {
+					poped_c = pop_adv_char_stack(c_stack);
 				}
 				break;
-			case ':':
+			case ':':    // Time to separate key and its value
+				if (c != '"') {
+					isKey = false;
+				}
 				break;
 			case ',':
+				if (c != '"') {
+					isKey = true;
+				}
 				break;
 			default:
+				if (c == '"') {
+					tstring(ts, json_str[i]);
+				}
 				break;
 		}
 		if (invalid) {
